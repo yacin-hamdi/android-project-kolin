4dd041b6c0aba07213c150d63f095952bc836c91
yacin-hamdi <yacin.ha9@gmail.com>
2024-05-21 20:43:48 +0100

    add a navigationIcon to topAppBar

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AppBar.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AppBar.kt
@@ -1,24 +1,46 @@
 package eu.tutorials.mywishlistapp
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlin.math.max
+
+
+
+
 
 @Composable
 fun AppBarView(
     title: String,
     onBackNavClicked: () -> Unit={}
 ){
+
+    val navigationIcon:(@Composable () -> Unit)? =
+        {
+            IconButton(onClick = { onBackNavClicked() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                    tint = Color.White,
+                    contentDescription = null
+                )
+            }
+        }
     TopAppBar(title = {
         Text(
-            text=title, 
+            text=title,
             color = colorResource(id = R.color.white),
             modifier = Modifier
                 .padding(start = 4.dp)
@@ -27,5 +49,6 @@ fun AppBarView(
     },
         elevation = 3.dp,
         backgroundColor = colorResource(id = R.color.app_bar_color),
+        navigationIcon = navigationIcon
         )
 }
