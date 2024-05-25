ebdc80d767574106b50e33bec8e87da7edc0f0db
yacin-hamdi <yacin.ha9@gmail.com>
2024-05-25 21:01:08 +0100

    set the value of wishViewModel repository

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/WishViewModel.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/WishViewModel.kt
@@ -13,7 +13,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
 class WishViewModel(
-    private val wishRepository: WishRepository
+    private val wishRepository: WishRepository = Graph.wishRepository
 ):ViewModel() {
     var wishTitleState by mutableStateOf("")
     var wishDescriptionState by mutableStateOf("")
