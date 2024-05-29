08ce4e7ffec0c17dcdaeb343981cccddc9e75a83
yacin-hamdi <yacin.ha9@gmail.com>
2024-05-29 21:10:01 +0100

    finish the app

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/HomeView.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/HomeView.kt
@@ -1,7 +1,10 @@
 package eu.tutorials.mywishlistapp
 
 import android.widget.Toast
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,19 +12,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.rememberDismissState
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -35,6 +47,7 @@ import eu.tutorials.mywishlistapp.data.DummyWish
 import eu.tutorials.mywishlistapp.data.Wish
 
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeView(navController: NavHostController,
              wishViewModel: WishViewModel){
@@ -64,12 +77,50 @@ fun HomeView(navController: NavHostController,
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)) {
-            items(wishList.value){
+            items(wishList.value, key = {wish -> wish.id}){
                 item ->
-                WishItem(wish = item) {
-                    val id = item.id
-                    navController.navigate(Screen.AddScreen.route + "/$id")
-                }
+                val dismissState = rememberDismissState(
+                    confirmStateChange = {
+                        if(it == DismissValue.DismissedToEnd || it == DismissValue.DismissedToStart){
+                            wishViewModel.deleteWish(item)
+                        }
+                        true
+                    }
+                )
+
+                SwipeToDismiss(
+                    state = dismissState,
+                    background = {
+                                 val color by animateColorAsState(
+                                     if(dismissState.dismissDirection == DismissDirection.EndToStart)
+                                     Color.Red else Color.Transparent,
+                                     label = ""
+                                 )
+
+                        val alignment = Alignment.CenterEnd
+                        Box(
+                           Modifier.fillMaxSize().background(color).padding(horizontal=20.dp),
+                            contentAlignment = alignment
+                        ){
+                            Icon(Icons.Default.Delete,
+                                contentDescription = "Delete Icon",
+                                tint = Color.White)
+                        }
+                    },
+                    directions = setOf(DismissDirection.EndToStart, DismissDirection.StartToEnd),
+                    dismissThresholds = {FractionalThreshold(0.25f)},
+                    dismissContent = {
+                        WishItem(wish = item) {
+                            val id = item.id
+                            navController.navigate(Screen.AddScreen.route + "/$id")
+                        }
+
+                    }
+
+
+
+                    )
+
             }
 
         }
