156cad9f56987c6225fa04f949e51b4bb08b7196
yacin-hamdi <yacin.ha9@gmail.com>
2024-05-30 19:34:38 +0100

    create musicAppUI project

---
+++ musicappui/app/src/main/java/eu/tutorials/MusicAppUI/MainActivity.kt
@@ -0,0 +1,28 @@
+package eu.tutorials.MusicAppUI
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import eu.tutorials.MusicAppUI.ui.theme.MyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
