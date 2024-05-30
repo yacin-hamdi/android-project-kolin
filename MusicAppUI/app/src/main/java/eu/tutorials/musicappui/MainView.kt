4f70958643f3b26dd3f90d324d3bb59cb7e42cd0
yacin-hamdi <yacin.ha9@gmail.com>
2024-05-30 20:36:03 +0100

    create a scaffold with topBar

---
+++ MusicAppUI/app/src/main/java/eu/tutorials/musicappui/MainView.kt
@@ -0,0 +1,42 @@
+package eu.tutorials.musicappui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainView(){
+    val scaffoldState:ScaffoldState = rememberScaffoldState()
+    val scope: CoroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Home")},
+                navigationIcon = {IconButton(onClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }) {
+                    Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
+
+                }})
+        }
+    ) {
+        Text(text = "hello", modifier = Modifier.padding(it))
+    }
+}
